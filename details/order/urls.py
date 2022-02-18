0b553e4e96494c0ec329c847b047c38afe9adc93
Niravkadia <niravrajput0008@gmail.com>
2022-02-18 16:44:13 +0530

    commit

--- details/order/urls.py
+++ details/order/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from . import views
+from order import views
 from django.urls import path,include
 
 urlpatterns = [
-    path('',views.insert),
-    path('show',views.show)
+    path('',views.insert,name='insert'),
+    path('show',views.show,name='show'),
+    path('delete/<int:id>',views.delete)
 ]
 
--- details/order/views.py
+++ details/order/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from customer.models import customer
 from order.models import order1
 from product.models import product
 from order.forms import orderform
+from customer.forms import customerform
 from order.models import order1
+
+
 def insert(request):
     if request.method=="POST":
         form=orderform (request.POST)
@@ -15,18 +18,19 @@ def insert(request):
             except:
                 pass
     form=orderform()
-    cus=customer.objects.all()
-    pro=product.objects.all()
-    
-    return render(request,'index.html',{'form':form,'cus':cus,'pro':pro})
-
-
+    form1=customerform()
+    return render(request,'index.html',{'form':form})
 
 
 def show(request):
     orders=order1.objects.all()
 
-    return render(request,'show.html',{'order1':orders})
+    return render(request,'show.html',{'order':orders})
 
 
 
+def delete(request,id):
+    order2=order1.objects.get(id=id)
+    order2.delete()
+
+    return redirect('/show')
