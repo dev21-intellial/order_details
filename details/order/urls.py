d914d1159ae8cf58676d0c20913550f02087a38a
Niravkadia <niravrajput0008@gmail.com>
2022-02-21 14:11:19 +0530

    done

--- details/order/urls.py
+++ details/order/urls.py
@@ -5,6 +5,8 @@ from django.urls import path,include
 urlpatterns = [
     path('',views.insert,name='insert'),
     path('show',views.show,name='show'),
-    path('delete/<int:id>',views.delete)
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('update/<int:id>',views.update,name='update')
 ]
 
--- details/order/views.py
+++ details/order/views.py
@@ -10,18 +10,25 @@ from product.forms import productform
 from order.models import order1
 
 
+
 def insert(request):
     if request.method=="POST":
-        form=orderform (request.POST)
-        if form.is_valid:
-            try:
-                form.save()
-                return redirect('/show')
-            except:
-                pass
-    form=orderform()
-    return render(request,'index.html',{'form':form})
-
+        
+        customer_id=request.POST['first_name']
+        product_id=request.POST['name']
+        qty=request.POST['qty']
+        unit_price=request.POST['unit_price']
+        total_price=request.POST['total_price']
+        data_store=order1(customer_id=customer_id,product_id=product_id,qty=qty,unit_price=unit_price,total_price=total_price)
+        data_store.save()
+        
+        return redirect('/show')
+        
+    else:
+        orders=order1.objects.all()
+        pro=product.objects.all()
+        cust=customer.objects.all()
+        return render(request,'insert.html',{'order':orders,'pro':pro,'cust':cust})
 
 def show(request):
     orders=order1.objects.all()
@@ -35,3 +42,23 @@ def delete(request,id):
     order2.delete()
 
     return redirect('/show')
+
+
+
+
+def edit(request,id):
+    edit1 = order1.objects.get(id=id)
+    edit2 = product.objects.all()
+    edit3 = customer.objects.all()
+    return render(request,'edit.html',{'edit1':edit1,'edit2':edit2,'edit3':edit3})
+
+def update(request,id):
+    customer_id=request.POST['first_name']
+    product_id=request.POST['name']
+    qty=request.POST['qty']
+    unit_price=request.POST['unit_price']
+    total_price=request.POST['total_price']
+    data_store=order1(customer_id=customer_id,product_id=product_id,qty=qty,unit_price=unit_price,total_price=total_price)
+    data_store.save()
+    return redirect('/show')
+
